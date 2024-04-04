971ac0fff3117a8772bf46271286b27e79ec7ed5
mikaelri <mikael.rintala@outlook.com>
2024-04-04 16:30:28 +0300

    update app window size

--- quiz-app/src/index.py
+++ quiz-app/src/index.py
@@ -5,6 +5,10 @@ def main():
     window = Tk()
     window.title("Quiz Application")
 
+    window.geometry("800x600")
+    window.minsize(800, 600)
+    window.maxsize(800, 600)
+
     ui_view = UI(window)
     ui_view.start()
 
